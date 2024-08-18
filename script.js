e2ed74409795c8abb972112ebc46f395c128e583
Jamil-4khtar <156568813+Jamil-4khtar@users.noreply.github.com>
2024-08-19 01:30:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -16,6 +16,6 @@ closeModalButton.addEventListener('click', function() {
 // When the user clicks anywhere outside of the modal, close it
 window.addEventListener('click', function(event) {
     if (event.target == modal) {
-        modal.style.display = "none";
+        modal.style.display = "block";
     }
 });
